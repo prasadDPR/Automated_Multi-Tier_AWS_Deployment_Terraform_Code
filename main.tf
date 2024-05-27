8a669c5f5e086e29564e1ce97c9f7356f0041ea8
Prasad <121819069+prasadDPR@users.noreply.github.com>
2024-05-27 09:13:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "aws-tfstate-file"
+    organization = "aws-statetf-org"
 
     workspaces {
       name = "cicd-aws-project"

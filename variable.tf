470ea67f935164b1bce4fa922387866da697820c
Prasad <121819069+prasadDPR@users.noreply.github.com>
2024-05-27 14:18:54 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,13 +2,3 @@ variable "private_key_path" {
   description = "Path to the SSH private key file"
   default     = "C:/Users/Dell/key/aws_us.pem"
 }
-
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-}

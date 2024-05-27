ba6b9143d4f5ed6fd1faa002b34ae547adc0be6e
Prasad <121819069+prasadDPR@users.noreply.github.com>
2024-05-27 13:38:36 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "us-east-1"
 
-  aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
-  aws-secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+  aws-access-key-id: secrets.AWS_ACCESS_KEY_ID
+  aws-secret-access-key: secrets.AWS_SECRET_ACCESS_KEY
 }

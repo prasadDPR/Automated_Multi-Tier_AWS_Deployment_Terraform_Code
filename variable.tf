182b3c38e5d5e4f9a62f4d0ae991621a42487df7
Prasad <121819069+prasadDPR@users.noreply.github.com>
2024-06-22 10:53:39 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,4 +1,4 @@
 variable "private_key_path" {
   description = "Path to the SSH private key file"
-  default     = "C:/Users/Dell/key/aws_us.pem"
+  default     = "C:/Users/Dell/key/aws_bank_key.pem"
 }

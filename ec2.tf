5f40e3e8e91a383962475e16f2f41a3dc1145304
Prasad <121819069+prasadDPR@users.noreply.github.com>
2024-06-22 10:52:51 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "pubic-server-1" {
   }
   ami               = "ami-04e5276ebb8451442"
   instance_type     = "t2.micro"
-  key_name          = "aws_us"
+  key_name          = "aws_bank_key"
   subnet_id         = aws_subnet.publicsubnet1a.id
   security_groups   = [aws_security_group.public-sg.id]
   availability_zone = "us-east-1a"
@@ -16,7 +16,7 @@ resource "aws_instance" "public-server-2" {
   }
   ami               = "ami-04e5276ebb8451442"
   instance_type     = "t2.micro"
-  key_name          = "aws_us"
+  key_name          = "aws_bank_key"
   subnet_id         = aws_subnet.publicsubnet1b.id
   security_groups   = [aws_security_group.public-sg.id]
   availability_zone = "us-east-1b"
